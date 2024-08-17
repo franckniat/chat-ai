ce167f226e6c496e33c71d2fd96e079b0bbf3e16
franckniat <niatmanuel2002@gmail.com>
2024-08-17 15:55:30 +0100

    style: apply style on active chat with current pathname

--- components/header/chat-list.tsx
+++ components/header/chat-list.tsx
@@ -1,33 +1,39 @@
-"use client"
-import { useChatList } from '@/providers/chat-list';
-import * as React from 'react';
+"use client";
+import { useChatList } from "@/providers/chat-list";
+import { Chat } from "@prisma/client";
+import Link from "next/link";
+import * as React from "react";
+import { usePathname } from "next/navigation";
 
-interface ChatProps {
-    title?: string;
-    href?: string;
-}
-
-export default function ChatList({
-    chats
-}: {
-    chats?: ChatProps[]
-}) {
-    const { isOpen, toggle } = useChatList();
-
-    return (
-        <aside className={`w-[300px] h-[calc(100vh-65px)] bottom-0 border-r border-foreground/5 bg-background/85 backdrop-blur fixed transition-transform duration-200 ${isOpen ? "-translate-x-full" : ""}`}>
-            <div className="flex items-center justify-between h-[60px] px-4 border-b border-foreground/5">
-                <h2 className="font-medium text-foreground/80">Vos discussions</h2>
-            </div>
-            <div className='px-4 py-2 max-h-[calc(100vh-125px)] overflow-y-auto'>
-                <ul>
-                    {chats?.map((chat, index) => (
-                        <li key={index} className="px-2 py-3 border-b border-foreground/20">
-                            <a href={chat.href} className="text-lg font-medium">{chat.title}</a>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        </aside>
-    )
+export default function ChatList({ chats }: { chats?: Chat[] }) {
+	const { isOpen, toggle } = useChatList();
+	const pathname = usePathname();
+	return (
+		<aside
+			className={`w-[300px] h-[calc(100vh-65px)] bottom-0 border-r border-foreground/5 bg-background/85 backdrop-blur fixed transition-transform duration-200 ${
+				isOpen ? "-translate-x-full" : ""
+			}`}
+		>
+			<div className="flex items-center justify-between h-[60px] px-4 border-b border-foreground/5">
+				<h2 className="font-medium text-foreground/80">
+					Vos discussions
+				</h2>
+			</div>
+			<div className="px-4 py-2 max-h-[calc(100vh-125px)] overflow-y-auto">
+				<div className="flex flex-col gap-1">
+					{chats?.map((chat, index) => (
+						<Link
+							href={`/chat/${chat.id}`}
+							key={index}
+							className={`text-sm font-medium hover:bg-foreground/5 rounded-md px-3 py-3 ${
+								pathname === `/chat/${chat.id}` && "bg-foreground/5"
+							}`}
+						>
+							<span>{chat.name.replace('"',"")}</span>
+						</Link>
+					))}
+				</div>
+			</div>
+		</aside>
+	);
 }
