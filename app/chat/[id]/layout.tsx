05abda1034b5360db424623f193cc65f6f7eacba
franckniat <niatmanuel2002@gmail.com>
2024-08-17 15:56:53 +0100

    refactor: remove api routes due to typesafe issues

--- app/chat/[id]/layout.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import FormChat from '@/components/chat/form-chat';
-
-export default function ChatLayout({ children }: { children: React.ReactNode }) {
-    return (
-        <>
-        </>
-    )
-}
