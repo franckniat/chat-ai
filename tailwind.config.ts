8a5bd95ac49e0299591069f8b33a0cf68c6f23fd
franckniat <niatmanuel2002@gmail.com>
2024-08-17 19:12:47 +0100

    config screens on tailwindCSS config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,12 @@ const config = {
 	],
   prefix: "",
   theme: {
+    screens:{
+      "sm": "640px",
+      "md": "768px",
+      "lg": "1024px",
+      "xl": "1280px",
+    },
     container: {
       center: true,
       padding: "2rem",
