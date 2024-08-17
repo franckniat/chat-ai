d2023ae125968ce17aed8f5a027b6494ce1c30e7
franckniat <niatmanuel2002@gmail.com>
2024-08-17 15:52:18 +0100

    feat: fetch messages on chat page

--- app/chat/[id]/page.tsx
+++ app/chat/[id]/page.tsx
@@ -1,15 +1,22 @@
 import React from 'react'
-import { auth } from '@/auth'
-import { signOut } from '@/auth';
-import { Button } from '@/components/ui/button';
-import { revalidatePath } from 'next/cache';
+import { getUser } from '@/lib/user';
+import { notFound, redirect } from 'next/navigation';
+import { getChatById } from '@/actions/chat';
+import { getMessagesBychatId } from '@/actions/message';
+import MessagesContent from '@/components/chat/messages';
 
 
-export default async function ChatPage({ id }: { id: string | undefined | null }) {
-    const user = await auth();
+export default async function ChatPage({params}:{params:{id: string}}) {
+    const user = await getUser();
+    if(!user) {
+        notFound();
+    }
+    const { id } = params;
+    const chat = await getChatById(id);
+    const messages = await getMessagesBychatId(chat!.id)
     return (
         <>
-            
+            <MessagesContent chatID={id} messages={messages}/>
         </>
     )
 }
