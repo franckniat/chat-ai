9e1a39deaf2fc02d89b40b275a697f2ad0fa778e
franckniat <niatmanuel2002@gmail.com>
2024-08-17 15:50:34 +0100

    feat: fetch chats list

--- app/chat/layout.tsx
+++ app/chat/layout.tsx
@@ -1,12 +1,17 @@
 import Navigation from '@/components/header'
 import ChatList from '@/components/header/chat-list';
 import React from 'react';
+import { getUser } from '@/lib/user';
+import { getChats } from '@/actions/chat';
+import { Chat } from '@prisma/client';
 
 export default async function ChatsLayout({children}:{children:React.ReactNode}) {
+  const user = await getUser();
+  const chats = await getChats(user!.id as string)
   return (
     <>
         <Navigation/>
-        <ChatList/>
+        <ChatList chats={chats as Chat[]}/>
         <section className="pt-[90px] pb-[160px] max-w-[800px] mx-auto">
             {children}
         </section>
